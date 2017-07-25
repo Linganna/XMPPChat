8db6b2791c880b440935cc244c76ee20188777b6
LInganna <linganna.a@synchronoss.com>
2017-07-25 16:15:40 +0530

    Fixed issue in connection

--- XMPPChat/XMPPConnection.swift
+++ XMPPChat/XMPPConnection.swift
@@ -11,21 +11,21 @@ import XMPPFramework
 
 open class XMPPConnection:NSObject{
     var xmppStream: XMPPStream
-
+    
     let hostName: String
     let userJID: XMPPJID
     let hostPort: UInt16
     let password: String
     
     var xmppDelegate:XMPPDelegate?
-
+    
     
     public init(hostName: String, userJIDString: String, hostPort: UInt16 = 5222, password: String, delegate:XMPPDelegate) {
         
         self.hostName = hostName
         self.hostPort = hostPort
         self.password = password
-
+        
         let userJID = XMPPJID(string: userJIDString)
         
         self.xmppStream = XMPPStream()
@@ -34,9 +34,10 @@ open class XMPPConnection:NSObject{
         self.userJID = userJID!
         self.xmppStream.startTLSPolicy = XMPPStreamStartTLSPolicy.allowed
         self.xmppDelegate = delegate
+        self.xmppStream.myJID = self.userJID
         super.init()
         self.xmppStream.addDelegate(self, delegateQueue: DispatchQueue.main)
-
+        
     }
     
     public func connect() {
@@ -46,7 +47,7 @@ open class XMPPConnection:NSObject{
         
         try! self.xmppStream.connect(withTimeout: XMPPStreamTimeoutNone)
     }
-
+    
     public func disconnect(){
         goOffline()
         self.xmppStream.disconnect()
@@ -63,7 +64,7 @@ open class XMPPConnection:NSObject{
         let presence = XMPPPresence(type: "unavailable")
         self.xmppStream.send(presence)
     }
-
+    
     
     public func sendMessage(message:String, to:String) {
         let message = "Yo!"
@@ -71,7 +72,10 @@ open class XMPPConnection:NSObject{
         let msg = XMPPMessage(type: "chat", to: senderJID)
         
         msg?.addBody(message)
+        self.xmppStream.addDelegate(self, delegateQueue: DispatchQueue.main)
+        
         self.xmppStream.send(msg)
+        
         
     }
     
@@ -84,12 +88,16 @@ extension XMPPConnection: XMPPStreamDelegate {
         print("Stream: Connected")
         try! stream.authenticate(withPassword: self.password)
     }
+    public func xmppStreamDidDisconnect(_ sender: XMPPStream!, withError error: Error!) {
+        print("Stream: DisCOnnected")
+        
+    }
     
     public func xmppStreamDidAuthenticate(_ sender: XMPPStream!) {
-        self.xmppStream.send(XMPPPresence())
         print("Stream: Authenticated")
-        self.xmppDelegate?.xmppConnectionState(status: true)
         goOnline()
+        self.xmppDelegate?.xmppConnectionState(status: true)
+        
     }
     
     
@@ -98,14 +106,16 @@ extension XMPPConnection: XMPPStreamDelegate {
         return false
     }
     
-    func xmppStream(sender: XMPPStream!, didReceiveMessage message: XMPPMessage!) {
-        print("Did receive message \(message)")
-        Messages.saveMessage(serverMsg: message, isOutGoing: false)
-    }
-    
-    func xmppStream(sender: XMPPStream!, didSendMessage message: XMPPMessage!) {
+    public func xmppStream(_ sender: XMPPStream!, didSend message: XMPPMessage!) {
         print("Did send message \(message)")
+        Messages.saveMessage(serverMsg: message, isOutGoing: true)
     }
     
-
+    
+    public func xmppStream(_ sender: XMPPStream!, didReceive message: XMPPMessage!) {
+        print("Did receive message \(message)")
+    }
+    
+    
 }
+
